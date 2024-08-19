ebe77ff134c1924671e678974bd187cbdbb617e4
marian1090 <mariann1042@gmail.com>
2024-08-18 22:17:19 -0300

    funciones obtener un libro e insertar

--- src/controller.js
+++ src/controller.js
@@ -12,6 +12,58 @@ class LibrosController {
       res.status(500).json({ error: "Error al obtener los libros" });
     }
   }
+
+  // funcion para obtener un libro
+  async getOne(req, res) {
+    const { id } = req.params;
+
+    try {
+      const [rows] = await pool.query("SELECT * FROM libros WHERE id = ?", [
+        id,
+      ]);
+      if (rows.length === 0) {
+        return res.status(404).json({ error: "Libro no encontrado" });
+      }
+
+      res.json(rows[0]);
+    } catch (error) {
+      console.error("Error al obtener el libro:", error.message);
+      res.status(500).json({ error: "Error al obtener el libro" });
+    }
+  }
+
+  // funcion para insertar libro
+  async create(req, res) {
+    const { nombre, autor, categoria, año_publicacion, ISBN } = req.body;
+
+    console.log("Datos recibidos:", req.body);
+
+    if (!nombre || !autor || !categoria || !año_publicacion || !ISBN) {
+      return res.status(400).json({ error: "Todos los campos son necesarios" });
+    }
+    try {
+      const [result] = await pool.query(
+        "INSERT INTO libros (nombre, autor, categoria, año_publicacion, ISBN) VALUES (?, ?, ?, ?, ?)",
+        [nombre, autor, categoria, año_publicacion, ISBN]
+      );
+
+      const newBookId = result.insertId;
+
+      res.status(201).json({
+        id: newBookId,
+        nombre,
+        autor,
+        categoria,
+        año_publicacion,
+        ISBN,
+        message: "Libro creado exitosamente",
+      });
+    } catch (error) {
+      console.error("Error al crear el libro:", error.message);
+
+      res.status(500).json({ error: "Error al crear el libro" });
+    }
+  }
 }
 
 export const libro = new LibrosController();
--- src/routes.js
+++ src/routes.js
@@ -5,3 +5,9 @@ export const router = Router();
 
 //ruta para llamar a todos los libros
 router.get("/getAll", libro.getAll);
+
+// Ruta para obtener un libro específico por ID
+router.get("/getOne/:id", libro.getOne);
+
+// Ruta para insetar un libro
+router.post("/create", libro.create);
